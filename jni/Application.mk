40276af4032073f0700dc2b5f82d90763f4c1c31
Daniel Pocock <daniel@pocock.com.au>
2013-07-29 21:25:55 +0200

    Add support for more platforms

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,3 +1,12 @@
 NDK_TOOLCHAIN_VERSION=4.4.3
 APP_PROJECT_PATH := $(shell pwd)
 APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/Android.mk
+# all platforms:
+#APP_ABI := all
+APP_ABI := armeabi
+APP_ABI += armeabi-v7a
+APP_ABI += x86
+# doesn't currently compile for mips, error about libz.so
+#APP_ABI += mips
+APP_PLATFORM := android-9
+
